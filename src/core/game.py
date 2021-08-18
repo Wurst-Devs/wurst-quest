c6d0d184fd136d7f33df4baba0bd37a4caad771a
Carl <carl.cochet@gmail.com>
2021-08-18 16:01:33 +0200

    Update requirements and load resources into the game

--- src/core/game.py
+++ src/core/game.py
@@ -1,14 +1,21 @@
 import logging
+import pandas as pd
+import os
 
 from .models import Action, State
 
 
 class Game:
     def __init__(self) -> None:
-        pass
+        self.monsters = None
+        self.equipments = None
 
     def init(self) -> None:
         logging.info("loading data...")
+
+        self.monsters = pd.read_csv(os.path.join("res", "monsters.csv"))
+        self.equipments = pd.read_csv(os.path.join("res", "equipments.csv"))
+
         # TODO
 
     def new_game(self) -> State:
