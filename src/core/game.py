d954cdc2a66a47f674eede2d5ed2c04fae4cb592
Carl <carl.cochet@gmail.com>
2021-08-18 19:34:01 +0200

    Add Content class to store game resources and new adjectives resource

--- src/core/game.py
+++ src/core/game.py
@@ -5,16 +5,22 @@ import os
 from .models import Action, State
 
 
+class Content:
+    def __init__(self):
+        self.monsters = pd.read_csv(os.path.join("res", "monsters.csv"))
+        self.equipments = pd.read_csv(os.path.join("res", "equipments.csv"))
+        self.adjectives = pd.read_csv(os.path.join("res", "adjectives.csv"))
+
+
 class Game:
     def __init__(self) -> None:
         self.monsters = None
         self.equipments = None
+        self.content = None
 
     def init(self) -> None:
         logging.info("loading data...")
-
-        self.monsters = pd.read_csv(os.path.join("res", "monsters.csv"))
-        self.equipments = pd.read_csv(os.path.join("res", "equipments.csv"))
+        self.content = Content()
 
         # TODO
 
