6401a1f3b317a39833fd42f443b8968cbabde6aa
Klemek <klemek@outlook.fr>
2021-08-31 16:49:33 +0200

    working game

--- src/wurst_quest/core/game.py
+++ src/wurst_quest/core/game.py
@@ -1,9 +1,9 @@
 import logging
 
 from .models import Action, State
-from .factories import MonsterFactory
+from .factories import MonsterFactory, PlayerFactory
 from .content import Content
-from .enums import Location, ActionType
+from .enums import Location, ActionType, PlayerGear
 
 
 class Game:
@@ -24,15 +24,15 @@ class Game:
         logging.info("loading data...")
         self.content.init()
 
-        # TODO
-
     def new_game(self) -> State:
         logging.info("new game:")
-        return State(
+        state = State(
             location=Location.CITY,
-            duration=Game.TIME_LONG,
+            duration=Game.TIME_NORMAL,
             status="Starting new adventure",
         )
+        state.player = PlayerFactory(state.random).new_player()
+        return state
 
     def compute(self, state: State, action: Action) -> State:
         state = state.clone()
@@ -51,6 +51,7 @@ class Game:
                 duration=Game.TIME_NORMAL,
                 status="Healing",
                 monster=None,
+                locked=False,
             )
         elif action.type == ActionType.BUY:
             state.update(
@@ -58,6 +59,7 @@ class Game:
                 duration=Game.TIME_LONG,
                 status="Buying things",
                 monster=None,
+                locked=False,
             )
             # TODO
         elif action.type == ActionType.LEAVE:
@@ -66,6 +68,7 @@ class Game:
                 duration=Game.TIME_NORMAL,
                 status="Leaving the city",
                 monster=None,
+                locked=False,
             )
         else:
             state.update(
@@ -73,25 +76,40 @@ class Game:
                 duration=Game.TIME_QUICK,
                 status="Waiting",
                 monster=None,
+                locked=False,
             )
         return state
 
     def _compute_outside(self, state: State, action: Action) -> State:
-        if action.type == ActionType.RETREAT:
+        can_level_up = PlayerFactory(state.random).can_level_up(state.player)
+        if can_level_up and state.locked:
+            state.player = PlayerFactory(state.random).level_up(state.player)
+            state.update(
+                location=Location.OUTSIDE,
+                duration=Game.TIME_QUICK,
+                status="Leveled up",
+                monster=None,
+                locked=False,
+            )
+        elif action.type == ActionType.RETREAT:
             state.update(
                 location=Location.CITY,
                 duration=Game.TIME_NORMAL,
                 status="Going back to the city",
                 monster=None,
+                locked=False,
             )
         elif action.type == ActionType.SEARCH:
             if state.random.randrange(0, 99) < Game.ENCOUNTER_CHANCE:
-                monster = MonsterFactory(state.random)
+                monster = MonsterFactory(state.random).generate_monster(
+                    state.player.level
+                )
                 state.update(
                     location=Location.BATTLE,
                     duration=Game.TIME_QUICK,
                     status=f"A wild {monster.name} appears",
                     monster=monster,
+                    locked=False,
                 )
             else:
                 state.update(
@@ -99,6 +117,7 @@ class Game:
                     duration=Game.TIME_NORMAL,
                     status=f"Nothing found while searching",
                     monster=None,
+                    locked=False,
                 )
         else:
             state.update(
@@ -106,31 +125,34 @@ class Game:
                 duration=Game.TIME_QUICK,
                 status="Waiting",
                 monster=None,
+                locked=False,
             )
         return state
 
     def _compute_battle(self, state: State, action: Action) -> State:
         if state.monster.hp <= 0:
-            if action == ActionType.SEARCH:
+            if not state.locked:
+                state.player.experience += state.monster.experience_gain
+                state.update(
+                    location=Location.BATTLE,
+                    duration=Game.TIME_NORMAL,
+                    status=f"Defeated {state.monster.name}, searching body",
+                    locked=True,
+                )
+            else:
                 loot = MonsterFactory(state.random).generate_loot(state.monster)
-                if loot[0] is not None:
+                if loot is not None:
                     state.player.inventory += [loot]
-                    status_text = "Found {loot[0]}"
+                    status_text = f"Found {loot.name}, searching for another monster"
                 else:
-                    status_text = "Found nothing"
+                    status_text = "Found nothing, searching for another monster"
+                can_level_up = PlayerFactory(state.random).can_level_up(state.player)
                 state.update(
                     location=Location.OUTSIDE,
                     duration=Game.TIME_NORMAL,
                     status=status_text,
                     monster=None,
-                )
-            else:
-                state.player.experience += state.monster.experience_gain
-                state.update(
-                    location=Location.OUTSIDE,
-                    duration=Game.TIME_NORMAL,
-                    status=f"Defeated {state.monster.name}, searching the body",
-                    monster=None,
+                    locked=can_level_up,
                 )
         elif state.player.hp <= 0:
             state.update(
@@ -138,26 +160,28 @@ class Game:
                 duration=Game.TIME_LONG,
                 status=f"Defeated by {state.monster.name}, going back to city",
                 monster=None,
+                locked=False,
             )
         else:
             # TODO equippment
-            player_attack = 1
+            player_attack = state.player.equipped[PlayerGear.WEAPON].value
             monster_attack = state.monster.attack
             # TODO handle first hit
             state.monster.hp -= player_attack
             status_text = f"{state.monster.name} takes {player_attack}"
             if state.monster.hp > 0:
                 state.player.hp -= monster_attack
-                status_text = f" and retaliates with {monster_attack}"
+                status_text += f" and retaliates with {monster_attack}"
             else:
-                status_text = f" and dies"
-            if action == ActionType.FLEE:
+                status_text += f" and dies"
+            if action.type == ActionType.FLEE:
                 if state.random.randrange(0, 99) < Game.FLEE_CHANCE:
                     state.update(
                         location=Location.OUTSIDE,
                         duration=Game.TIME_NORMAL,
                         status=f"Fleeing combat",
                         monster=None,
+                        locked=False,
                     )
                     return state
                 else:
@@ -169,5 +193,6 @@ class Game:
                 location=Location.BATTLE,
                 duration=Game.TIME_QUICK,
                 status=status_text,
+                locked=False,
             )
         return state
