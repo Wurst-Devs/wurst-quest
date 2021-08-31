961f300aa794c80c7e4659bfb0575cad27c0546c
Klemek <klemek@outlook.fr>
2021-08-31 16:28:20 +0200

    basic autoplay

--- src/wurst_quest/core/autoplay.py
+++ src/wurst_quest/core/autoplay.py
@@ -1,5 +1,5 @@
 from .models import Action, State
-from .enums import ActionType
+from .enums import ActionType, Location
 
 
 class Autoplay:
@@ -7,4 +7,13 @@ class Autoplay:
         pass
 
     def next_action(self, state: State) -> Action:
-        return Action(ActionType.WAIT)
+        if state.locked:
+            return Action(ActionType.WAIT)
+        elif state.location == Location.CITY:
+            return Action(ActionType.LEAVE)
+        elif state.location == Location.OUTSIDE:
+            return Action(ActionType.SEARCH)
+        elif state.location == Location.BATTLE:
+            return Action(ActionType.BATTLE)
+        else:
+            return Action(ActionType.WAIT)
